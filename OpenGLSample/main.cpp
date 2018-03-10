2612172a943a1aedd869594c9504a924bf3abaed
Dinh Tran <dinh-tran@cybozu.vn>
2018-03-10 10:29:58 +0700

    Initial Lab one

--- OpenGLSample/main.cpp
+++ OpenGLSample/main.cpp
@@ -3,96 +3,77 @@
 #endif
 
 #ifdef __APPLE__
-#include <GLUT/glut.h>
 #include <OpenGL/gl.h>
 #include <OpenGL/glu.h>
+#include <GLUT/glut.h>
 #else
 #include <GL/glut.h>
-#include <GL/glu.h>
-#include <GL/gl.h>
 #endif
 
 #include <string.h>
 #include <cstdlib>
 
-void myInit()
-{
-    glClearColor(1.0,1.0,1.0,0.0);
-    glColor3f(0.0f, 0.0f, 0.0f);
-    glPointSize(4.0);
-    glMatrixMode(GL_PROJECTION);
-    glLoadIdentity();
-    gluOrtho2D(0.0, 640.0, 0.0, 480.0);
-}
-
-void drawText(int xPos, int yPos, char str[])
-{
-    glRasterPos2i(xPos, yPos); //Position
-    void * font = GLUT_BITMAP_9_BY_15;
-    for (int i=0; i<strlen(str);i++)
-    {
-        glutBitmapCharacter(font, str[i]);
-    }
-}
-
-void myDisplay()
-{
-    glClear(GL_COLOR_BUFFER_BIT);
-    drawText(100, 300, "OpenGL - OPEN GRAPHICS LANGUAGE");
-    glFlush();
-}
-
-// Constants and variables...
-// Menu item constants
-enum {RED_ITEM, GREEN_ITEM, BLUE_ITEM, QUIT_ITEM};
-
-// Menu callback: choice is value of the chosen menu entry
-void menu(int choice) {
-    switch(choice) {
-        case RED_ITEM:
-            glColor3f(1.0, 0.0, 0.0); // Red
-            myDisplay();
-            break;
-        case GREEN_ITEM:
-            glColor3f(0.0, 1.0, 0.0); // Green
-            myDisplay();
-            break;
-        case BLUE_ITEM:
-            glColor3f(0.0, 0.0, 1.0); // Blue
-            myDisplay();
-            break;
-        case QUIT_ITEM:
-            exit(0);
-            break;
-        default:
-            break;
-    }
-}
-
-// Create the menu, add the items, and attach to the mouse button
-void createMenu() {
-    // create menu
-    int m = glutCreateMenu(menu); // simultaneously registers callback
-
-    //add entries: 2nd parameter is value returned when entry is selected
-    glutAddMenuEntry("Red",RED_ITEM);
-    glutAddMenuEntry("Green",GREEN_ITEM);
-    glutAddMenuEntry("Blue",BLUE_ITEM);
-    glutAddMenuEntry("Quit",QUIT_ITEM);
-    
-    // attach button to menu: overrides mouse callback for RIGHT button
-    glutAttachMenu(GLUT_RIGHT_BUTTON);
-}
+void display();
+void init();
 
 int main(int argc, char** argv) {
-    glutInit(&argc, (char**)argv); //initialize the tool kit
-    glutInitDisplayMode(GLUT_SINGLE |GLUT_RGB);//set the display mode
-    glutInitWindowSize(640, 480); //set window size
-    glutInitWindowPosition(100, 150); //set window position on screen
-    glutCreateWindow("Output Text"); //open the screen window
-    createMenu();
-    glutDisplayFunc(myDisplay);
-    myInit();
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+    glutInitWindowSize(600, 400);
+    glutInitWindowPosition(100, 150);
+    glutCreateWindow("LAB 1 - (Bai 1)");
+    
+    glutDisplayFunc(display);
+    init();
     glutMainLoop();
     return 0;
 }
+
+GLfloat v0[2] = {1,1};
+GLfloat v1[2] = {2,3};
+GLfloat v2[2] = {4,1};
+GLfloat v3[2] = {6,2};
+GLfloat v4[2] = {9,3};
+GLfloat v5[2] = {7,5};
+
+void init() {
+    glClearColor(1.0, 1.0, 1.0, 1.0);
+    
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    gluOrtho2D(0.0, 9.0, 0.0, 6.0);
+}
+
+void drawGrid() {
+    glColor3f(0.6f, 0.6f, 0.6f);
+    glLineWidth(1.0);
+    glBegin(GL_LINES);
+    for (int i = 0; i < 9; i++) {
+        glVertex2i(i, 0);
+        glVertex2i(i, 6);
+    }
+    for (int i = 0; i < 6; i++) {
+        glVertex2i(0, i);
+        glVertex2i(9, i);
+    }
+    glEnd();
+}
+
+void display() {
+    glClear(GL_COLOR_BUFFER_BIT);
+    drawGrid();
+    glLineWidth(4.0);
+    glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+    glColor3f(0.0f, 0.0f, 0.0f);
+    glBegin(GL_TRIANGLES);
+        glVertex2fv(v0);
+        glVertex2fv(v1);
+        glVertex2fv(v2);
+        glVertex2fv(v3);
+        glVertex2fv(v4);
+        glVertex2fv(v5);
+    glEnd();
+    glFlush();
+}
+
+
