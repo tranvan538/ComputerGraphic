c850b30c1cac1c6970cd39f326ec835aa78781f2
Dinh Tran <dinh-tran@cybozu.vn>
2018-03-11 16:13:57 +0700

    Fix title

--- OpenGLSample/main.cpp
+++ OpenGLSample/main.cpp
@@ -21,7 +21,7 @@ int main(int argc, char** argv) {
     glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
     glutInitWindowSize(600, 400);
     glutInitWindowPosition(100, 150);
-    glutCreateWindow("LAB 1 - (Bai 1)");
+    glutCreateWindow("LAB 1 - (Bai 3)");
     
     glutDisplayFunc(display);
     init();
