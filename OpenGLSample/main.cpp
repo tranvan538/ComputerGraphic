4c4f1b8e728f3f04591ee515dbb3f1803f2d4705
dinh-tran <dinh-tran@cybozu.vn>
2018-03-11 18:23:06 +0700

    Allow run on visual studio

--- OpenGLSample/main.cpp
+++ OpenGLSample/main.cpp
@@ -1,98 +1,96 @@
-#ifdef WIN32
-#include <windows.h>
-#endif
-
 #ifdef __APPLE__
 #include <GLUT/glut.h>
 #include <OpenGL/gl.h>
 #include <OpenGL/glu.h>
 #else
-#include <GL/glut.h>
-#include <GL/glu.h>
-#include <GL/gl.h>
+#include <windows.h>
+#include "stdafx.h"
+#include <GL/GL.h>
+#include <GL/GLU.h>
+#include <glut.h>
 #endif
+
+#define WIDTH         700
+#define HEIGHT        400
+#define SQUARE        25
 
 #include <string.h>
 #include <cstdlib>
 
-void myInit()
-{
-    glClearColor(1.0,1.0,1.0,0.0);
-    glColor3f(0.0f, 0.0f, 0.0f);
-    glPointSize(4.0);
-    glMatrixMode(GL_PROJECTION);
-    glLoadIdentity();
-    gluOrtho2D(0.0, 640.0, 0.0, 480.0);
-}
+void display();
+void init();
 
-void drawText(int xPos, int yPos, char str[])
-{
-    glRasterPos2i(xPos, yPos); //Position
-    void * font = GLUT_BITMAP_9_BY_15;
-    for (int i=0; i<strlen(str);i++)
-    {
-        glutBitmapCharacter(font, str[i]);
-    }
-}
-
-void myDisplay()
-{
-    glClear(GL_COLOR_BUFFER_BIT);
-    drawText(100, 300, "OpenGL - OPEN GRAPHICS LANGUAGE");
-    glFlush();
-}
-
-// Constants and variables...
-// Menu item constants
-enum {RED_ITEM, GREEN_ITEM, BLUE_ITEM, QUIT_ITEM};
-
-// Menu callback: choice is value of the chosen menu entry
-void menu(int choice) {
-    switch(choice) {
-        case RED_ITEM:
-            glColor3f(1.0, 0.0, 0.0); // Red
-            myDisplay();
-            break;
-        case GREEN_ITEM:
-            glColor3f(0.0, 1.0, 0.0); // Green
-            myDisplay();
-            break;
-        case BLUE_ITEM:
-            glColor3f(0.0, 0.0, 1.0); // Blue
-            myDisplay();
-            break;
-        case QUIT_ITEM:
-            exit(0);
-            break;
-        default:
-            break;
-    }
-}
-
-// Create the menu, add the items, and attach to the mouse button
-void createMenu() {
-    // create menu
-    int m = glutCreateMenu(menu); // simultaneously registers callback
-
-    //add entries: 2nd parameter is value returned when entry is selected
-    glutAddMenuEntry("Red",RED_ITEM);
-    glutAddMenuEntry("Green",GREEN_ITEM);
-    glutAddMenuEntry("Blue",BLUE_ITEM);
-    glutAddMenuEntry("Quit",QUIT_ITEM);
-    
-    // attach button to menu: overrides mouse callback for RIGHT button
-    glutAttachMenu(GLUT_RIGHT_BUTTON);
-}
-
+#ifdef __APPLE__
 int main(int argc, char** argv) {
-    glutInit(&argc, (char**)argv); //initialize the tool kit
-    glutInitDisplayMode(GLUT_SINGLE |GLUT_RGB);//set the display mode
-    glutInitWindowSize(640, 480); //set window size
-    glutInitWindowPosition(100, 150); //set window position on screen
-    glutCreateWindow("Output Text"); //open the screen window
-    createMenu();
-    glutDisplayFunc(myDisplay);
-    myInit();
-    glutMainLoop();
-    return 0;
+#else
+int _tmain(int argc, _TCHAR* argv[])
+#endif
+{
+	glutInit(&argc, (char**)argv);
+
+	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+	glutInitWindowSize(WIDTH, HEIGHT);
+	glutInitWindowPosition(100, 150);
+	glutCreateWindow("LAB 1 - (Bai 11)");
+
+	glutDisplayFunc(display);
+	init();
+	glutMainLoop();
+	return 0;
+}
+
+void init() {
+	glClearColor(1.0, 1.0, 1.0, 1.0);
+
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	gluOrtho2D(0.0, WIDTH / 2, 0.0, HEIGHT / 2);
+}
+
+void display() {
+	glClear(GL_COLOR_BUFFER_BIT);
+	glLineWidth(4.0);
+	glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+	glColor3f(0.0f, 0.0f, 0.0f);
+
+	GLint y = 0;
+	GLint x = 0;
+
+	glLineWidth(2.0);
+	glBegin(GL_LINES);
+	while (y < HEIGHT) {
+		glVertex2i(x, y);
+		glVertex2i(HEIGHT, y);
+		y += SQUARE;
+	}
+	glEnd();
+
+	x = 0;
+	y = 0;
+	glBegin(GL_QUADS);
+	int z = 0;
+	while (y < HEIGHT) {
+		if (z % 2 == 0) {
+			x = SQUARE / 3;
+		}
+		else {
+			x = 2 * SQUARE / 3;
+		}
+
+		if (z % 4 == 3) {
+			x = 0;
+		}
+
+		while (x < WIDTH) {
+			glVertex2i(x, y);
+			glVertex2i(x + SQUARE, y);
+			glVertex2i(x + SQUARE, y + SQUARE);
+			glVertex2i(x, y + SQUARE);
+			x += 2 * SQUARE;
+		}
+		y += SQUARE;
+		z++;
+	}
+	glEnd();
+	glFlush();
 }
